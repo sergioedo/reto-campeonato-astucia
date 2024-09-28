b8c7aebb2fa88bd278f00b40ea4c033f3715e60d
sergioedo <sergio.edo@gmail.com>
2024-09-28 10:52:26 +0200

    fix astro ouput mode for Vercel Serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'server',  // Aquí especificamos que el output es server-side
   adapter: vercel(),
 });
